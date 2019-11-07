679bf148dfb021ff9b26c3720aaec164df5c2d3d
Nate Clark <natec425@gmail.com>
2019-11-07 12:23:50 -0600

    Fix tests

--- app/tests.py
+++ app/tests.py
@@ -16,13 +16,25 @@ class TestHomePage(TestCase):
         for coffee in coffees:
             self.assertContains(response, coffee.name)
 
+    def test_has_a_button_to_buy_each_coffee(self):
+        coffees = [
+            models.Coffee.objects.create(name="Test Coffee 1", price=1),
+            models.Coffee.objects.create(name="Test Coffee 2", price=3),
+            models.Coffee.objects.create(name="Test Coffee 3", price=7),
+        ]
+
+        response = self.client.get(reverse("home"))
+
+        for coffee in coffees:
+            self.assertContains(response, f"Buy {coffee.name}")
+
 
 # Create your tests here.
 class TestBuyingACoffee(TestCase):
     def test_creates_a_transaction(self):
         coffee_to_buy = models.Coffee.objects.create(name="Test Coffee", price=2.85)
 
-        self.client.post(reverse("buy_coffee", coffee_to_buy.id))
+        self.client.post(reverse("buy_coffee", args=[coffee_to_buy.id]))
 
         self.assertEqual(
             coffee_to_buy.transaction_set.count(),
@@ -30,21 +42,38 @@ class TestBuyingACoffee(TestCase):
             "buying a coffee should create a new Transaction for that coffee",
         )
 
-        transaction = coffee_to_buy.transaction_set.first()
+        transaction = coffee_to_buy.transaction_set.last()
 
         self.assertAlmostEqual(
-            transaction.pre_tax, 2.85, "pre tax should match the price of the coffee"
+            transaction.pre_tax,
+            2.85,
+            msg="pre tax should match the price of the coffee",
         )
 
         self.assertAlmostEqual(
-            transaction.tax, 0.24, "tax should be 7% of the coffee's price"
+            transaction.tax, 0.2, places=2, msg="tax should be 7% of the coffee's price"
         )
 
     def test_redirects_to_the_transaction_page(self):
         coffee_to_buy = models.Coffee.objects.create(name="Test Coffee", price=2.85)
 
-        response = self.client.post(reverse("buy_coffee", coffee_to_buy.id))
+        response = self.client.post(reverse("buy_coffee", args=[coffee_to_buy.id]))
 
-        transaction = coffee_to_buy.transaction_set()
+        transaction = coffee_to_buy.transaction_set.last()
 
-        self.assertRedirects(response, reverse("transaction_detail", transaction.id))
+        self.assertRedirects(
+            response, reverse("transaction_detail", args=[transaction.id])
+        )
+
+
+class TestTransactionPage(TestCase):
+    def test_shows_transaction_information(self):
+        coffee_to_buy = models.Coffee.objects.create(name="Test Coffee", price=2.85)
+        transaction = coffee_to_buy.transaction_set.create(pre_tax=123, tax=456)
+
+        response = self.client.get(reverse("transaction_detail", args=[transaction.id]))
+
+        self.assertContains(response, transaction.id)
+        self.assertContains(response, coffee_to_buy.name)
+        self.assertContains(response, transaction.pre_tax)
+        self.assertContains(response, transaction.tax)
